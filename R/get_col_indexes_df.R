7c1dc77b42a1a18b695a0bcac21eebe8c90c87bf
Kostas Tsolis <kostas.tsolis@zoho.com>
2017-06-28 20:17:03 +0200

    1

--- R/get_col_indexes_df.R
+++ R/get_col_indexes_df.R
@@ -1,7 +1,19 @@
-# check column names from a dataframa
-# find the indexes that match a specific pattern
-# x is a dataframe
-# id is a vector contaning character identifiers
+#' Get column indexes of a dataframe
+#'
+#' @description Reads a dataframe and returns the column indexes that match to a specific pattern.
+#' If input is not a dataframe, returns an error. You can set the first argument as.data.frame(x).
+#'
+#' @param x A dataframe to with the columns of interest
+#' @param id Vector contaning character identifiers for the columns of interest.
+#'
+#' @examples
+#' x <- data.frame(one = 1, two = 2, three = 3, four = 4)
+#' vect <- c("o", "wo")
+#'
+#' get_col_indexes(x, "wo")
+#' get_col_indexes(x, vect)
+#'
+#' @export
 
 get_col_indexes_df <- function(x, id) {
   if (any(class(x) %in% c("data.frame", "tbl", "tbl_df"))) {
