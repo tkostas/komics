58741d6f8bfdf9bd8ee1ac8d26c19a7061e360df
Kostas Tsolis <kostas.tsolis@zoho.com>
2017-06-30 12:14:23 +0200

    1

--- R/get_col_indexes_df.R
+++ R/get_col_indexes_df.R
@@ -24,15 +24,3 @@ get_col_indexes_df <- function(x, id) {
     stop("Input is not a dataframe. Set as.data.frame and run again.", call. = FALSE)
   }
 }
-
-# example
-# x <- data.frame(one = 1, two = 2, three = 3, four = 4)
-# vect <- c("o", "wo")
-#
-# get_col_indexes(x, "wo")
-# get_col_indexes(x, vect)
-#
-# y <- as.list(x)
-# get_col_indexes(y, vect)
-
-
