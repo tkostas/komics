56ce508a8f1869304d64cd15eca1e75a2351fcc0
Kostas Tsolis <kostas.tsolis@zoho.com>
2017-07-10 15:58:16 +0200

    1

--- R/calc_exp_df.R
+++ R/calc_exp_df.R
@@ -1,4 +1,37 @@
-calc_exp_df <- function(data,
+#' Calculate exponential values from a dataframe
+#'
+#' @description Reads a data frame and calculates the exponential value (basex) for
+#' the defined columns. Non-numeric columns can be excluded by adding the column name
+#' in exclusion vector. For partial matching of column names use
+#' \code{complete_match = FALSE}. See \code{calc_exp_vec}.
+#'
+#' @param x 	Input data frame. It can contain also non-numeric columns.
+#' @param base Base number for which you will calculate the exponential value.
+#' Default value is 2.
+#' @param sub Character string to substitute in column names. Default is empty
+#' @param replacement Character string to replace the substituted one. Default is empty.
+#' @param exclusion Vector containing labels from the columns that should be excluded.
+#'  Even a part of the label is enough.
+#' @param complete_match Logical with default value \code{TRUE}. If exclusion vector
+#' contains exactly the column names of the excluded columns, leave the
+#' \code{complete_match} to \code{TRUE}. For partial matching of the columns switch
+#' to \code{FALSE}.
+#' @param append Logical with default value \code{FALSE}. If set \code{TRUE} processed
+#' data will be added in the starting dataframe. If \code{TRUE}, the processed columned
+#' will be merged only with the excluded ones.
+#'
+#'
+#' @examples
+#' y1 <- calc_exp_df(x = my_data,
+#'                   base = 2,
+#'                   sub = "norm_log_",
+#'                   replacement = "norm_",
+#'                   exclusion = colnames(y)[1:3],
+#'                   complete_match = TRUE)
+#'
+#' @export
+
+calc_exp_df <- function(x,
                         base = 2,
                         sub = "",
                         replacement = "",
@@ -9,7 +42,7 @@ calc_exp_df <- function(data,
   print("Calculated exponential values ...")
   print("---------------------")
   # prepare exclusion list
-  excl_list <- exclude_cols(x = data, exclusion, complete_match)
+  excl_list <- exclude_cols(x = x, exclusion, complete_match)
   in_use_table <- excl_list$in_use_table
 
   # print checking if columns are numeric
@@ -36,3 +69,4 @@ calc_exp_df <- function(data,
   print(paste("Done! Ouput contains", n, "rows and", p, "columns."))
   return(output2)
 }
+
--- R/calc_exp_vec.R
+++ R/calc_exp_vec.R
@@ -1,16 +1,20 @@
-# calculate exponential base^x
+#' Calculate exponential base^x from a vector
+#' @description Calculate exponential basex. Reads a numeric
+#' element convert to the power specified.
+#'
+#' @param x Numeric vector.
+#' @param base Numeric element that you will raise \code{x}.
+#'
+#' @examples
+#' x <- 4
+#' base <- 2
+#' calc_exp(a, x)
+#'
+#' v <- seq(1:5)
+#' calc_exp(v)
+#' @export
+
 calc_exp_vec <- function(x, base = 2) {
   y <- base^x
   return(as.numeric(y))
 }
-
-
-#example
-
- # x <- 4
- # base <- 2
- #
- # calc_exp_vec(x, base)
- #
- # v <- seq(1:5)
- # calc_exp_vec(v)
