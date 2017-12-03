3d9cedec1892dae9bb823ba7c378c06c34a56a6c
Kostas Tsolis <kostas.tsolis@zoho.com>
2017-12-03 18:59:28 +0100

    1

--- R/calc_ttest_padj.R
+++ R/calc_ttest_padj.R
@@ -1,5 +1,5 @@
 #' Perform t-test and adjust p-values
-#' @description Reads a dataframe, recognises protein groups, frequencies within each group,
+#' @description Reads a dataframe, recognizes protein groups, frequencies within each group,
 #' performes t-test, calculates p-values, and adjusts p-value for multiple hypothesis testing error.
 #' The starting dataframe can also contain columns with text. Experimental groups are identified based
 #' on specific character string for each group. If missing values are converted to \code{NA}, they will
