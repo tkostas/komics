eb40f59ed86563158c31e23f6f1a6e15c0a4aab6
Kostas Tsolis <kostas.tsolis@zoho.com>
2017-12-12 00:29:10 +0100

    1

---
+++ R/plot_kwd_lines.R
@@ -0,0 +1,37 @@
+#' Plot keywords after enrichment test as lines
+#' @description Reads the output of \code{link[komics]{kwd_enrichment_multi}}, along with
+#'         a vector with the categories names and plots a linegraph of the keyword ratio between
+#'         test group and the background.
+#'         You don't have any options to modify the appearence of the graph, in this version.
+#'
+#' @param x A list containing the keyword enrichment output from \code{link[komics]{kwd_enrichment_multi}}
+#'        function. You remove elements of this list, as long as you don't change the column names.
+#' @param group_vec A vector containing the labels of each group. Should have the same length
+#'        with the \code{x} argument.
+#'
+#' @examples
+#' \donttest{
+#'
+#' plot_kwd_lines(x = kwd_enrichment_multi_output,
+#'                group_vec = categories_vector)
+#' }
+#' @export
+
+
+plot_kwd_lines <- function(x, group_vec){
+  for (i in seq_along(group_vec)){
+    print(paste("round:", i))
+    kwd_table <- x[[i]][,c("keyword", "ratio_test_group")]
+    kwd_table$group <- group_vec[i]
+    if (i == 1) {
+      output <- kwd_table
+    } else {
+      output <- rbind(output, kwd_table)
+    }
+  }
+  ggplot(output, aes(keyword, ratio_test_group, col = group, group = group)) +
+    geom_line(size = 2) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+}
